2674c02719ef7e70bd236b2f943669be10259ee9
FantasticFiasco <mattias.kindborg@gmail.com>
2019-05-19 11:45:44 +0200

    chore: add code coverage

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 # AWS.SignatureVersion4
 
 [![Build status](https://ci.appveyor.com/api/projects/status/bh71gd22ogf2ogvl/branch/master?svg=true)](https://ci.appveyor.com/project/FantasticFiasco/aws-signature-version-4)
+[![codecov](https://codecov.io/gh/FantasticFiasco/aws-signature-version-4/branch/master/graph/badge.svg)](https://codecov.io/gh/FantasticFiasco/aws-signature-version-4)
 [![License: Apache-2.0](https://img.shields.io/badge/license-Apache--2.0-blue.svg)]
--- build/build.sh
+++ build/build.sh
@@ -3,9 +3,21 @@
 echo -e "$(curl --silent https://raw.githubusercontent.com/FantasticFiasco/logo/master/logo.ansi)"
 echo
 
-echo "build: Build started"
+echo "build: build started"
 echo "build: dotnet cli v$(dotnet --version)"
 dotnet build -c Release
 
-echo "test: Test started"
-dotnet test -c Release
+echo "test: test started"
+coverlet ./test/bin/Release/netcoreapp2.1/AWS.SignatureVersion4.Test.dll \
+    --target "dotnet" \
+    --targetargs "test --configuration Release --no-build" \
+    --exclude "[xunit.*]*" \
+    --format opencover
+
+echo "test: upload coverage report"
+# Codecov expects environment variables CI and APPVEYOR to be "True", while it on AppVeyor Ubuntu
+# images are specified as "true" (see https://www.appveyor.com/docs/environment-variables/ for
+# reference). Lets remedy this by settig the environment variables ourself.
+export CI="True"
+export APPVEYOR="True"
+bash <(curl -s https://codecov.io/bash)
