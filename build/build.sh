59cca01444bae6deca6b0e0555a2e46a738b1286
FantasticFiasco <mattias.kindborg@gmail.com>
2019-05-23 19:53:55 +0200

    wip

--- build/build.sh
+++ build/build.sh
@@ -7,18 +7,24 @@ set -e
 echo -e "$(curl --silent https://raw.githubusercontent.com/FantasticFiasco/logo/master/logo.ansi)"
 echo
 
+# --- VARIABLES ---
+TAGGED_BUILD=$(( "$APPVEYOR_REPO_TAG" == "true" ? true : false ))
+GIT_SHA="${APPVEYOR_REPO_COMMIT:0:7}"
+echo "[info] triggered by git tag: $TAGGED_BUILD"
+echo "[info] git sha: $GIT_SHA"
+
 # --- BUILD STAGE ---
-echo "build: build started"
-echo "build: dotnet cli v$(dotnet --version)"
+echo "[build] build started"
+echo "[build] dotnet cli v$(dotnet --version)"
 dotnet build -c Release
 
 # --- TEST STAGE ---
-echo "test: test started"
+echo "[test] test started"
 
 # Exclude integration tests if we run as part of a pull requests. Integration tests rely on
 # secrets, which are omitted by AppVeyor on pull requests.
 if [ $APPVEYOR_PULL_REQUEST_NUMBER ]; then
-    echo "test: skip integration tests on pull requests"
+    echo "[test] skip integration tests on pull requests"
     TEST_FILTER="--filter Category!=Integration"
 fi
 
@@ -29,7 +35,7 @@ coverlet ./test/bin/Release/netcoreapp2.1/AWS.SignatureVersion4.Test.dll \
     --exclude "[xunit.*]*" \
     --format opencover
 
-echo "test: upload coverage report"
+echo "[test] upload coverage report"
 
 # Codecov expects environment variables CI and APPVEYOR to be "True", while it on AppVeyor Ubuntu
 # images are specified as "true" (see https://www.appveyor.com/docs/environment-variables/ for
