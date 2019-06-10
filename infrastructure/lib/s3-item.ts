f661bc55b3028a96abc1b112de660ac8afc5f0fd
FantasticFiasco <mattias.kindborg@gmail.com>
2019-06-10 20:35:03 +0200

    wip

--- infrastructure/lib/s3-item.ts
+++ infrastructure/lib/s3-item.ts
@@ -13,7 +13,18 @@ export class S3Item extends Construct {
         super(scope, id);
 
         new AwsCustomResource(this, 'PutObject', {
-            onCreate: {
+            onDelete: {
+                action: 'deleteObject',
+                parameters: {
+                    Bucket: props.bucket.bucketName,
+                    Key: props.key,
+                },
+                // Update physical id to always put the latest version
+                physicalResourceId: Date.now().toString(),
+                service: 'S3',
+            },
+            // Update is also called when resource is created
+            onUpdate: {
                 action: 'putObject',
                 parameters: {
                     Body: props.value,
