c53c4f18207ca4b639da52cf35b4c76624ddbf9d
FantasticFiasco <mattias.kindborg@gmail.com>
2019-07-12 10:46:06 +0200

    wip

--- build/build.ps1
+++ build/build.ps1
@@ -8,11 +8,11 @@ Write-Host "$LOGO" -ForegroundColor Green
 # VARIABLES
 # -------------------------------------------------------------------------------------------------
 $GIT_SHA = "$env:APPVEYOR_REPO_COMMIT".substring(0, 7)
-Write-Host "$GIT_SHA"
+$IS_TAGGED_BUILD = If ("${APPVEYOR_REPO_TAG}" -eq "true") { $true } Else { $false }
+Write-Host "$IS_TAGGED_BUILD"
 
 
 
-# GIT_SHA="${APPVEYOR_REPO_COMMIT:0:7}"
 # [ "${APPVEYOR_REPO_TAG}" = "true" ] && IS_TAGGED_BUILD=true || IS_TAGGED_BUILD=false
 # [ ! -z "${APPVEYOR_PULL_REQUEST_NUMBER}" ] && IS_PULL_REQUEST=true || IS_PULL_REQUEST=false
 # echo "[info] git sha: ${GIT_SHA}"
