bfb014c0a4e950ae32764cf4f6ea9a66566e2fc5
FantasticFiasco <mattias.kindborg@gmail.com>
2020-06-21 12:39:27 +0200

    wip

--- test/Integration/S3/GetAsyncShould.cs
+++ test/Integration/S3/GetAsyncShould.cs
@@ -1,5 +1,6 @@
 ﻿using System.Net;
 using System.Net.Http;
+using System.Threading;
 using System.Threading.Tasks;
 using AwsSignatureVersion4.Integration.ApiGateway.Authentication;
 using Shouldly;
@@ -19,12 +20,9 @@ namespace AwsSignatureVersion4.Integration.S3
         [InlineData(IamAuthenticationType.Role)]
         public async Task SucceedGivenNoPrefix(IamAuthenticationType iamAuthenticationType)
         {
-            // Arrange
-            var url = $"{Context.S3Url}{Bucket.Foo.Key}";
-
             // Act
             var response = await HttpClient.GetAsync(
-                url,
+                $"{Context.S3Url}{Bucket.Foo.Key}",
                 Context.RegionName,
                 Context.ServiceName,
                 ResolveCredentials(iamAuthenticationType));
@@ -39,12 +37,9 @@ namespace AwsSignatureVersion4.Integration.S3
         [InlineData(IamAuthenticationType.Role)]
         public async Task SucceedGivenPrefix(IamAuthenticationType iamAuthenticationType)
         {
-            // Arrange
-            var url = $"{Context.S3Url}{Bucket.Foo.Bar.Key}";
-
             // Act
             var response = await HttpClient.GetAsync(
-                url,
+                $"{Context.S3Url}{Bucket.Foo.Bar.Key}",
                 Context.RegionName,
                 Context.ServiceName,
                 ResolveCredentials(iamAuthenticationType));
@@ -59,12 +54,9 @@ namespace AwsSignatureVersion4.Integration.S3
         [InlineData(IamAuthenticationType.Role)]
         public async Task SucceedGivenDeepPrefix(IamAuthenticationType iamAuthenticationType)
         {
-            // Arrange
-            var url = $"{Context.S3Url}{Bucket.Foo.Bar.Baz.Key}";
-
             // Act
             var response = await HttpClient.GetAsync(
-                url,
+                $"{Context.S3Url}{Bucket.Foo.Bar.Baz.Key}",
                 Context.RegionName,
                 Context.ServiceName,
                 ResolveCredentials(iamAuthenticationType));
@@ -72,6 +64,91 @@ namespace AwsSignatureVersion4.Integration.S3
             // Assert
             response.StatusCode.ShouldBe(HttpStatusCode.OK);
             (await response.Content.ReadAsStringAsync()).ShouldBe(Bucket.Foo.Bar.Baz.Content);
+        }
+
+        [Theory]
+        [InlineData(IamAuthenticationType.User)]
+        [InlineData(IamAuthenticationType.Role)]
+        public async Task SucceedGivenHttpCompletionOption(IamAuthenticationType iamAuthenticationType)
+        {
+            // Arrange
+            var completionOption = HttpCompletionOption.ResponseContentRead;
+
+            // Act
+            var response = await HttpClient.GetAsync(
+                $"{Context.S3Url}{Bucket.Foo.Key}",
+                completionOption,
+                Context.RegionName,
+                Context.ServiceName,
+                ResolveCredentials(iamAuthenticationType));
+
+            // Assert
+            response.StatusCode.ShouldBe(HttpStatusCode.OK);
+            (await response.Content.ReadAsStringAsync()).ShouldBe(Bucket.Foo.Content);
+        }
+
+        [Theory]
+        [InlineData(IamAuthenticationType.User)]
+        [InlineData(IamAuthenticationType.Role)]
+        public async Task SucceedGivenCancellationToken(IamAuthenticationType iamAuthenticationType)
+        {
+            // Arrange
+            var ct = new CancellationToken();
+
+            // Act
+            var response = await HttpClient.GetAsync(
+                $"{Context.S3Url}{Bucket.Foo.Key}",
+                ct,
+                Context.RegionName,
+                Context.ServiceName,
+                ResolveCredentials(iamAuthenticationType));
+
+            // Assert
+            response.StatusCode.ShouldBe(HttpStatusCode.OK);
+            (await response.Content.ReadAsStringAsync()).ShouldBe(Bucket.Foo.Content);
+        }
+
+        [Theory]
+        [InlineData(IamAuthenticationType.User)]
+        [InlineData(IamAuthenticationType.Role)]
+        public void AbortGivenCanceled(IamAuthenticationType iamAuthenticationType)
+        {
+            // Arrange
+            var ct = new CancellationToken(true);
+
+            // Act
+            var task = HttpClient.GetAsync(
+                $"{Context.S3Url}{Bucket.Foo.Key}",
+                ct,
+                Context.RegionName,
+                Context.ServiceName,
+                ResolveCredentials(iamAuthenticationType));
+
+            // Assert
+            task.Status.ShouldBe(TaskStatus.Canceled);
+        }
+
+        [Theory]
+        [InlineData(IamAuthenticationType.User)]
+        [InlineData(IamAuthenticationType.Role)]
+        public async Task SucceedGivenHttpCompletionOptionAndCancellationToken(IamAuthenticationType iamAuthenticationType)
+        {
+            // Arrange
+            var completionOption = HttpCompletionOption.ResponseContentRead;
+            var ct = new CancellationToken();
+
+            // Act
+            var response = await HttpClient.GetAsync(
+                $"{Context.S3Url}{Bucket.Foo.Key}",
+                completionOption,
+                ct,
+                Context.RegionName,
+                Context.ServiceName,
+                ResolveCredentials(iamAuthenticationType));
+
+            // Assert
+            response.StatusCode.ShouldBe(HttpStatusCode.OK);
+            (await response.Content.ReadAsStringAsync()).ShouldBe(Bucket.Foo.Content);
         }
     }
 }
