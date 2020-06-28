ee2b3ded6b3ff2086a0d6ae13a4b23a859204d29
FantasticFiasco <mattias.kindborg@gmail.com>
2020-06-29 00:11:58 +0200

    wip

--- test/Integration/S3/SendAsyncShould.cs
+++ test/Integration/S3/SendAsyncShould.cs
@@ -54,29 +54,10 @@ namespace AwsSignatureVersion4.Integration.S3
         public async Task PassTestSuiteGivenUserWithPermissions(params string[] scenarioName)
         {
             // Arrange
-            if (scenarioName[0] == "get-unreserved" || scenarioName[0] == "get-vanilla-query-unreserved")
-            {
-                await Bucket.PutObjectAsync("-._~0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz");
-            }
-            else if (scenarioName[0] == "get-utf8" || scenarioName[0] == "get-vanilla-utf8-query")
-            {
-                await Bucket.PutObjectAsync("ሴ");
-            }
-            else if (scenarioName.Length == 2 && scenarioName[0] == "normalize-path" && scenarioName[1] == "get-slashes")
-            {
-                await Bucket.PutObjectAsync("/example//");
-            }
-            else if (scenarioName.Length == 2 && scenarioName[0] == "normalize-path" && scenarioName[1] == "get-space")
-            {
-                await Bucket.PutObjectAsync("example space/");
-            }
-            else
-            {
-                await Bucket.PutObjectAsync("/");
-            }
-
             var request = BuildRequest(scenarioName);
             var authenticationType = IamAuthenticationType.User;
+
+            await UploadRequiredObjectAsync(scenarioName);
 
             // Act
             var response = await HttpClient.SendAsync(
@@ -89,22 +70,55 @@ namespace AwsSignatureVersion4.Integration.S3
             response.StatusCode.ShouldBe(HttpStatusCode.OK);
         }
 
-        private HttpRequestMessage BuildRequest(string[] scenarioName)
+        [Theory]
+        [InlineData("get-header-key-duplicate")]
+        [InlineData("get-header-value-multiline")]
+        [InlineData("get-header-value-order")]
+        [InlineData("get-header-value-trim")]
+        [InlineData("get-unreserved")]
+        [InlineData("get-utf8")]
+        [InlineData("get-vanilla")]
+        [InlineData("get-vanilla-empty-query-key")]
+        [InlineData("get-vanilla-query")]
+        [InlineData("get-vanilla-query-order-key")]
+        [InlineData("get-vanilla-query-order-key-case")]
+        [InlineData("get-vanilla-query-order-value")]
+        [InlineData("get-vanilla-query-unreserved")]
+        [InlineData("get-vanilla-utf8-query")]
+        [InlineData("normalize-path", "get-relative")]
+        [InlineData("normalize-path", "get-relative-relative")]
+        [InlineData("normalize-path", "get-slash")]
+        [InlineData("normalize-path", "get-slash-dot-slash")]
+        [InlineData("normalize-path", "get-slashes")]
+        [InlineData("normalize-path", "get-slash-pointless-dot", Skip = SkipReasons.NotSupportedByS3)]
+        [InlineData("normalize-path", "get-space")]
+        [InlineData("post-header-key-case", Skip = SkipReasons.NotSupportedByS3)]
+        [InlineData("post-header-key-sort", Skip = SkipReasons.NotSupportedByS3)]
+        [InlineData("post-header-value-case", Skip = SkipReasons.NotSupportedByS3)]
+        [InlineData("post-sts-token", "post-sts-header-after", Skip = SkipReasons.NotSupportedByS3)]
+        [InlineData("post-sts-token", "post-sts-header-before", Skip = SkipReasons.NotSupportedByS3)]
+        [InlineData("post-vanilla", Skip = SkipReasons.NotSupportedByS3)]
+        [InlineData("post-vanilla-empty-query-value", Skip = SkipReasons.NotSupportedByS3)]
+        [InlineData("post-vanilla-query", Skip = SkipReasons.NotSupportedByS3)]
+        [InlineData("post-x-www-form-urlencoded", Skip = SkipReasons.NotSupportedByS3)]
+        [InlineData("post-x-www-form-urlencoded-parameters", Skip = SkipReasons.NotSupportedByS3)]
+        public async Task PassTestSuiteGivenAssumedRole(params string[] scenarioName)
         {
-            var request = testSuiteContext.LoadScenario(scenarioName).Request;
+            // Arrange
+            var request = BuildRequest(scenarioName);
+            var authenticationType = IamAuthenticationType.Role;
 
-            // Redirect the request to the AWS API Gateway
-            request.RequestUri = request.RequestUri
-                .ToString()
-                .Replace("https://example.amazonaws.com", Context.S3BucketUrl)
-                .ToUri();
+            await UploadRequiredObjectAsync(scenarioName);
 
-            // The "Host" header is now invalid since we redirected the request to the AWS API
-            // Gateway. Lets remove the header and have the signature implementation re-add it
-            // correctly.
-            request.Headers.Remove("Host");
+            // Act
+            var response = await HttpClient.SendAsync(
+                request,
+                Context.RegionName,
+                Context.ServiceName,
+                ResolveCredentials(authenticationType));
 
-            return request;
+            // Assert
+            response.StatusCode.ShouldBe(HttpStatusCode.OK);
         }
 
         [Theory]
@@ -128,6 +142,48 @@ namespace AwsSignatureVersion4.Integration.S3
 
             // Assert
             response.StatusCode.ShouldBe(HttpStatusCode.OK);
+        }
+
+        private async Task UploadRequiredObjectAsync(params string[] scenarioName)
+        {
+            if (scenarioName[0] == "get-unreserved" || scenarioName[0] == "get-vanilla-query-unreserved")
+            {
+                await Bucket.PutObjectAsync("-._~0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz");
+            }
+            else if (scenarioName[0] == "get-utf8" || scenarioName[0] == "get-vanilla-utf8-query")
+            {
+                await Bucket.PutObjectAsync("ሴ");
+            }
+            else if (scenarioName.Length == 2 && scenarioName[0] == "normalize-path" && scenarioName[1] == "get-slashes")
+            {
+                await Bucket.PutObjectAsync("/example//");
+            }
+            else if (scenarioName.Length == 2 && scenarioName[0] == "normalize-path" && scenarioName[1] == "get-space")
+            {
+                await Bucket.PutObjectAsync("example space/");
+            }
+            else
+            {
+                await Bucket.PutObjectAsync("/");
+            }
+        }
+
+        private HttpRequestMessage BuildRequest(string[] scenarioName)
+        {
+            var request = testSuiteContext.LoadScenario(scenarioName).Request;
+
+            // Redirect the request to the AWS S3 bucket
+            request.RequestUri = request.RequestUri
+                .ToString()
+                .Replace("https://example.amazonaws.com", Context.S3BucketUrl)
+                .ToUri();
+
+            // The "Host" header is now invalid since we redirected the request to the AWS S3
+            // bucket. Lets remove the header and have the signature implementation re-add it
+            // correctly.
+            request.Headers.Remove("Host");
+
+            return request;
         }
     }
 }
