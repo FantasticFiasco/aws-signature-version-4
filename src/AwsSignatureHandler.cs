4a3b09d0794a27bee39c1c43285646c3c42abdad
FantasticFiasco <mattias.kindborg@gmail.com>
2020-11-19 22:49:42 +0100

    wip

---
+++ src/AwsSignatureHandler.cs
@@ -0,0 +1,14 @@
+﻿using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace AwsSignatureVersion4
+{
+    public class AwsSignatureHandler : DelegatingHandler
+    {
+        protected override Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
+        {
+            return base.SendAsync(request, cancellationToken);
+        }
+    }
+}
---
+++ test/AwsSignatureHandlerTest.cs
@@ -0,0 +1,30 @@
+﻿using System.Net.Http;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace AwsSignatureVersion4
+{
+    public class AwsSignatureHandlerTest
+    {
+        private readonly ITestOutputHelper output;
+
+        public AwsSignatureHandlerTest(ITestOutputHelper output)
+        {
+            this.output = output;
+        }
+
+        [Fact]
+        public async void Test()
+        {
+            var second = new HttpClientHandler();
+            var first = new AwsSignatureHandler
+            {
+                InnerHandler = second
+            };
+
+            var client = new HttpClient(first);
+            var response = await client.GetStringAsync("https://www.google.com");
+            output .WriteLine(response);
+        }
+    }
+}
