7b12451326dd292bf41c81f6019dadba3cb9d367
FantasticFiasco <mattias.kindborg@gmail.com>
2020-11-22 17:28:42 +0100

    wip

---
+++ test/Integration/ApiGateway/AwsSignatureHandlerShould.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Amazon.Runtime;
+using AwsSignatureVersion4.Integration.ApiGateway.Authentication;
+using AwsSignatureVersion4.Private;
+using AwsSignatureVersion4.TestSuite;
+using Microsoft.Extensions.DependencyInjection;
+using Shouldly;
+using Xunit;
+
+namespace AwsSignatureVersion4.Integration.ApiGateway
+{
+    public class AwsSignatureHandlerShould : ApiGatewayIntegrationBase, IClassFixture<TestSuiteContext>
+    {
+        private readonly TestSuiteContext testSuiteContext;
+
+        public AwsSignatureHandlerShould(IntegrationTestContext context, TestSuiteContext testSuiteContext)
+            : base(context)
+        {
+            this.testSuiteContext = testSuiteContext;
+        }
+
+        [Theory]
+        [InlineData("get-header-key-duplicate")]
+        [InlineData("get-header-value-multiline")]
+        [InlineData("get-header-value-order")]
+        [InlineData("get-header-value-trim")]
+        [InlineData("get-unreserved")]
+        [InlineData("get-utf8")]
+        [InlineData("get-vanilla")]
+        [InlineData("get-vanilla-empty-query-key")]
+        [InlineData("get-vanilla-query")]
+        [InlineData("get-vanilla-query-order-key")]
+        [InlineData("get-vanilla-query-order-key-case")]
+        [InlineData("get-vanilla-query-order-value")]
+        [InlineData("get-vanilla-query-unreserved", Skip = SkipReasons.NotSupportedByApiGateway)]
+        [InlineData("get-vanilla-utf8-query")]
+        [InlineData("normalize-path", "get-relative")]
+        [InlineData("normalize-path", "get-relative-relative")]
+        [InlineData("normalize-path", "get-slash")]
+        [InlineData("normalize-path", "get-slash-dot-slash")]
+        [InlineData("normalize-path", "get-slashes")]
+        [InlineData("normalize-path", "get-slash-pointless-dot")]
+        [InlineData("normalize-path", "get-space")]
+        [InlineData("post-header-key-case")]
+        [InlineData("post-header-key-sort")]
+        [InlineData("post-header-value-case")]
+        [InlineData("post-sts-token", "post-sts-header-after")]
+        [InlineData("post-sts-token", "post-sts-header-before", Skip = SkipReasons.RedundantStsTokenScenario)]
+        [InlineData("post-vanilla")]
+        [InlineData("post-vanilla-empty-query-value")]
+        [InlineData("post-vanilla-query")]
+        [InlineData("post-x-www-form-urlencoded")]
+        [InlineData("post-x-www-form-urlencoded-parameters", Skip = SkipReasons.RedundantContentTypeCharset)]
+        public async Task PassTestSuiteGivenUserWithPermissions(params string[] scenarioName)
+        {
+            // Arrange
+            var request = BuildRequest(scenarioName);
+
+            ServiceCollection.AddTransient<AwsSignatureHandlerOptions>(_ => new AwsSignatureHandlerOptions(
+                Context.RegionName,
+                Context.ServiceName,
+                ResolveCredentials(IamAuthenticationType.User)));
+
+            using var httpClient = HttpClientFactory.CreateClient("integration");
+
+            // Act
+            var response = await httpClient.SendAsync(request);
+
+            // Assert
+            response.StatusCode.ShouldBe(HttpStatusCode.OK);
+        }
+
+        private HttpRequestMessage BuildRequest(string[] scenarioName)
+        {
+            var request = testSuiteContext.LoadScenario(scenarioName).Request;
+
+            // Redirect the request to the AWS API Gateway
+            request.RequestUri = request.RequestUri
+                .ToString()
+                .Replace("https://example.amazonaws.com", Context.ApiGatewayUrl)
+                .ToUri();
+
+            // The "Host" header is now invalid since we redirected the request to the AWS API
+            // Gateway. Lets remove the header and have the signature implementation re-add it
+            // correctly.
+            request.Headers.Remove("Host");
+
+            return request;
+        }
+
+
+
+        //[Fact]
+        //public async void Test()
+        //{
+        //    var services = new ServiceCollection();
+        //    services.AddTransient<AwsSignatureHandler>();
+        //    services.AddTransient<AwsSignatureHandlerOptions>(provider => new AwsSignatureHandlerOptions("", "", new ImmutableCredentials("a", "b", "c")));
+
+        //    services.AddHttpClient("test",
+        //            c =>
+        //            {
+        //                c.BaseAddress = new Uri("https://www.google.com");
+        //                c.DefaultRequestHeaders.Add("SOME-HEADER", "value");
+        //            })
+        //        .AddHttpMessageHandler<AwsSignatureHandler>();
+
+        //    var client = services
+        //        .BuildServiceProvider()
+        //        .GetService<IHttpClientFactory>()
+        //        .CreateClient("test");
+
+        //    var response = await client.GetStringAsync("/");
+        //}
+    }
+}
--- test/Integration/IntegrationBase.cs
+++ test/Integration/IntegrationBase.cs
@@ -2,6 +2,7 @@
 using System.Net.Http;
 using Amazon.Runtime;
 using AwsSignatureVersion4.Integration.ApiGateway.Authentication;
+using Microsoft.Extensions.DependencyInjection;
 using Xunit;
 
 namespace AwsSignatureVersion4.Integration
@@ -14,11 +15,32 @@ namespace AwsSignatureVersion4.Integration
             Context = context;
 
             HttpClient = new HttpClient();
+
+            ServiceCollection = new ServiceCollection();
+            ServiceCollection
+                .AddTransient<AwsSignatureHandler>()
+                .AddHttpClient("integration")
+                .AddHttpMessageHandler<AwsSignatureHandler>();
         }
 
         protected IntegrationTestContext Context { get; }
 
+        #region Properties serving tests that use HttpClient
+
         protected HttpClient HttpClient { get; }
+
+        #endregion
+
+        #region Properties serving tests that use IHttpClientFactory
+
+        protected IServiceCollection ServiceCollection { get; }
+
+        protected IHttpClientFactory HttpClientFactory =>
+            ServiceCollection
+                .BuildServiceProvider()
+                .GetService<IHttpClientFactory>();
+
+        #endregion
 
         public void Dispose() => HttpClient?.Dispose();
 
