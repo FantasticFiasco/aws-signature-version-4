f43a507f1561376e50aebe0446541113bc22b2e5
FantasticFiasco <mattias@kindb.org>
2021-06-09 18:56:48 +0200

    wip

--- src/Private/ErrorMessages.cs
+++ src/Private/ErrorMessages.cs
@@ -16,6 +16,13 @@ namespace AwsSignatureVersion4.Private
             "An invalid request URI was provided. The request URI must either be an absolute " +
             "URI or BaseAddress must be set.";
 
+        public const string InvalidRegionName =
+            "An invalid region name was provided. Please specify a valid region name, e.g. " +
+            "\"us-east-1\" for US East (N. Virginia).";
+        public const string InvalidServiceName =
+            "An invalid service name was provided. Please specify a valid service name, e.g. " +
+            "\"execute-api\" for the API Gateway.";
+
         public const string S3DoesNotSupportPost = "Uploading files to S3 using POST is not " +
             "supported by AWS, use PUT instead.";
     }
