01f23415b6d4390d047b5790e595571198f606cf
FantasticFiasco <mattias@kindb.org>
2021-07-29 11:48:07 +0200

    wip

--- src/DeleteAsyncExtensions.cs
+++ src/DeleteAsyncExtensions.cs
@@ -12,6 +12,57 @@ namespace System.Net.Http
     /// </summary>
     public static class DeleteAsyncExtensions
     {
+        #region DeleteAsync(string, string, string, <credentials>)
+
+        /// <summary>
+        /// Send a Signature Version 4 signed DELETE request to the specified Uri as an
+        /// asynchronous operation.
+        /// </summary>
+        /// <param name="self">
+        /// The extension target.
+        /// </param>
+        /// <param name="requestUri">
+        /// The Uri the request is sent to.
+        /// </param>
+        /// <param name="regionName">
+        /// The system name of the AWS region associated with the endpoint, e.g. "us-east-1".
+        /// </param>
+        /// <param name="serviceName">
+        /// The signing name of the service, e.g. "execute-api".
+        /// </param>
+        /// <param name="credentials">
+        /// AWS credentials containing the following parameters:
+        /// - The AWS public key for the account making the service call.
+        /// - The AWS secret key for the account making the call, in clear text.
+        /// - The session token obtained from STS if request is authenticated using temporary
+        ///   security credentials, e.g. a role.
+        /// </param>
+        /// <returns>
+        /// The task object representing the asynchronous operation.
+        /// </returns>
+        /// <exception cref="ArgumentNullException">
+        /// The <paramref name="requestUri"/> is null.
+        /// </exception>
+        /// <exception cref="HttpRequestException">
+        /// The request failed due to an underlying issue such as network connectivity, DNS
+        /// failure, server certificate validation or timeout.
+        /// </exception>
+        /// <remarks>
+        /// This operation will not block. The returned <see cref="Task{TResult}"/> object will
+        /// complete once the entire response including content is read.
+        /// </remarks>
+        public static Task<HttpResponseMessage> DeleteAsync(
+            this HttpClient self,
+            string requestUri,
+            string regionName,
+            string serviceName,
+            AWSCredentials credentials) =>
+            self.DeleteAsync(
+                requestUri.ToUri(),
+                regionName,
+                serviceName,
+                credentials);
+
         /// <summary>
         /// Send a Signature Version 4 signed DELETE request to the specified Uri as an
         /// asynchronous operation.
@@ -57,6 +108,60 @@ namespace System.Net.Http
             ImmutableCredentials credentials) =>
             self.DeleteAsync(
                 requestUri.ToUri(),
+                regionName,
+                serviceName,
+                credentials);
+
+        #endregion
+
+        #region DeleteAsync(Uri, string, string, <credentials>)
+
+        /// <summary>
+        /// Send a Signature Version 4 signed DELETE request to the specified Uri as an
+        /// asynchronous operation.
+        /// </summary>
+        /// <param name="self">
+        /// The extension target.
+        /// </param>
+        /// <param name="requestUri">
+        /// The Uri the request is sent to.
+        /// </param>
+        /// <param name="regionName">
+        /// The system name of the AWS region associated with the endpoint, e.g. "us-east-1".
+        /// </param>
+        /// <param name="serviceName">
+        /// The signing name of the service, e.g. "execute-api".
+        /// </param>
+        /// <param name="credentials">
+        /// AWS credentials containing the following parameters:
+        /// - The AWS public key for the account making the service call.
+        /// - The AWS secret key for the account making the call, in clear text.
+        /// - The session token obtained from STS if request is authenticated using temporary
+        ///   security credentials, e.g. a role.
+        /// </param>
+        /// <returns>
+        /// The task object representing the asynchronous operation.
+        /// </returns>
+        /// <exception cref="ArgumentNullException">
+        /// The <paramref name="requestUri"/> is null.
+        /// </exception>
+        /// <exception cref="HttpRequestException">
+        /// The request failed due to an underlying issue such as network connectivity, DNS
+        /// failure, server certificate validation or timeout.
+        /// </exception>
+        /// <remarks>
+        /// This operation will not block. The returned <see cref="Task{TResult}"/> object will
+        /// complete once the entire response including content is read.
+        /// </remarks>
+        public static Task<HttpResponseMessage> DeleteAsync(
+            this HttpClient self,
+            Uri requestUri,
+            string regionName,
+            string serviceName,
+            AWSCredentials credentials) =>
+            self.DeleteAsync(
+                requestUri,
+                CancellationToken.None,
                 regionName,
                 serviceName,
                 credentials);
@@ -111,6 +216,65 @@ namespace System.Net.Http
                 serviceName,
                 credentials);
 
+        #endregion
+
+        #region DeleteAsync(string, CancellationToken, string, string, <credentials>)
+
+        /// <summary>
+        /// Send a Signature Version 4 signed DELETE request to the specified Uri with a
+        /// cancellation token as an asynchronous operation.
+        /// </summary>
+        /// <param name="self">
+        /// The extension target.
+        /// </param>
+        /// <param name="requestUri">
+        /// The Uri the request is sent to.
+        /// </param>
+        /// <param name="cancellationToken">
+        /// A cancellation token that can be used by other objects or threads to receive notice of
+        /// cancellation.
+        /// </param>
+        /// <param name="regionName">
+        /// The system name of the AWS region associated with the endpoint, e.g. "us-east-1".
+        /// </param>
+        /// <param name="serviceName">
+        /// The signing name of the service, e.g. "execute-api".
+        /// </param>
+        /// <param name="credentials">
+        /// AWS credentials containing the following parameters:
+        /// - The AWS public key for the account making the service call.
+        /// - The AWS secret key for the account making the call, in clear text.
+        /// - The session token obtained from STS if request is authenticated using temporary
+        ///   security credentials, e.g. a role.
+        /// </param>
+        /// <returns>
+        /// The task object representing the asynchronous operation.
+        /// </returns>
+        /// <exception cref="ArgumentNullException">
+        /// The <paramref name="requestUri"/> is null.
+        /// </exception>
+        /// <exception cref="HttpRequestException">
+        /// The request failed due to an underlying issue such as network connectivity, DNS
+        /// failure, server certificate validation or timeout.
+        /// </exception>
+        /// <remarks>
+        /// This operation will not block. The returned <see cref="Task{TResult}"/> object will
+        /// complete once the entire response including content is read.
+        /// </remarks>
+        public static Task<HttpResponseMessage> DeleteAsync(
+            this HttpClient self,
+            string requestUri,
+            CancellationToken cancellationToken,
+            string regionName,
+            string serviceName,
+            AWSCredentials credentials) =>
+            self.DeleteAsync(
+                requestUri.ToUri(),
+                cancellationToken,
+                regionName,
+                serviceName,
+                credentials);
+
         /// <summary>
         /// Send a Signature Version 4 signed DELETE request to the specified Uri with a
         /// cancellation token as an asynchronous operation.
@@ -161,6 +325,65 @@ namespace System.Net.Http
             ImmutableCredentials credentials) =>
             self.DeleteAsync(
                 requestUri.ToUri(),
+                cancellationToken,
+                regionName,
+                serviceName,
+                credentials);
+
+        #endregion
+
+        #region DeleteAsync(Uri, CancellationToken, string, string, <credentials>)
+
+        /// <summary>
+        /// Send a Signature Version 4 signed DELETE request to the specified Uri with a
+        /// cancellation token as an asynchronous operation.
+        /// </summary>
+        /// <param name="self">
+        /// The extension target.
+        /// </param>
+        /// <param name="requestUri">
+        /// The Uri the request is sent to.
+        /// </param>
+        /// <param name="cancellationToken">
+        /// A cancellation token that can be used by other objects or threads to receive notice of
+        /// cancellation.
+        /// </param>
+        /// <param name="regionName">
+        /// The system name of the AWS region associated with the endpoint, e.g. "us-east-1".
+        /// </param>
+        /// <param name="serviceName">
+        /// The signing name of the service, e.g. "execute-api".
+        /// </param>
+        /// <param name="credentials">
+        /// AWS credentials containing the following parameters:
+        /// - The AWS public key for the account making the service call.
+        /// - The AWS secret key for the account making the call, in clear text.
+        /// - The session token obtained from STS if request is authenticated using temporary
+        ///   security credentials, e.g. a role.
+        /// </param>
+        /// <returns>
+        /// The task object representing the asynchronous operation.
+        /// </returns>
+        /// <exception cref="ArgumentNullException">
+        /// The <paramref name="requestUri"/> is null.
+        /// </exception>
+        /// <exception cref="HttpRequestException">
+        /// The request failed due to an underlying issue such as network connectivity, DNS
+        /// failure, server certificate validation or timeout.
+        /// </exception>
+        /// <remarks>
+        /// This operation will not block. The returned <see cref="Task{TResult}"/> object will
+        /// complete once the entire response including content is read.
+        /// </remarks>
+        public static Task<HttpResponseMessage> DeleteAsync(
+            this HttpClient self,
+            Uri requestUri,
+            CancellationToken cancellationToken,
+            string regionName,
+            string serviceName,
+            AWSCredentials credentials) =>
+            self.SendAsync(
+                new HttpRequestMessage(HttpMethod.Delete, requestUri),
                 cancellationToken,
                 regionName,
                 serviceName,
@@ -220,5 +443,7 @@ namespace System.Net.Http
                 regionName,
                 serviceName,
                 credentials);
+
+        #endregion
     }
 }
