e4c92e24a9797a6efed922c171f7ada8156e6ded
FantasticFiasco <mattias@kindb.org>
2022-06-17 20:18:44 +0200

    wip

--- infrastructure/lib/api-gateway/handlers/request.ts
+++ infrastructure/lib/api-gateway/handlers/request.ts
@@ -6,20 +6,30 @@ interface HttpResponse {
   body: string
 }
 
-const BODY = JSON.stringify({
-  firstName: 'John',
-  surname: 'Doe',
-  age: 42,
-})
+interface RequestDescription {
+  method: string
+  path: string
+  headers: { [name: string]: string | undefined } | null
+  queryParameters: { [name: string]: string | undefined } | null
+  body: string | null
+}
 
-export const handler = async (event: APIGatewayEvent): Promise<HttpResponse> => {
-  console.log(`Event: ${JSON.stringify(event, null, 2)}`);
+export const handler = async ({httpMethod, path, headers, queryStringParameters, body}: APIGatewayEvent): Promise<HttpResponse> => {
+  const requestDescription: RequestDescription = {
+    method: httpMethod,
+    path,
+    headers,
+    queryParameters: queryStringParameters,
+    body
+  }
+
+  console.log(requestDescription)
 
   return {
     statusCode: 200,
     headers: {
       'content-type': 'application/json',
     },
-    body: BODY,
+    body: JSON.stringify(requestDescription),
   }
 }
