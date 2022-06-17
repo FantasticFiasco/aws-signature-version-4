e087cc333be49a574f225af46d08cad28a69bc1d
FantasticFiasco <mattias@kindb.org>
2022-06-17 18:11:16 +0200

    wip

--- test/Integration/ApiGateway/Contents/Extensions.cs
+++ test/Integration/ApiGateway/Contents/Extensions.cs
@@ -2,13 +2,22 @@
 using System.Net.Http;
 using System.Text;
 using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
 
 namespace AwsSignatureVersion4.Integration.ApiGateway.Contents
 {
     public static class Extensions
     {
         public static string ToJson<T>(this T self) =>
-            JsonConvert.SerializeObject(self);
+            JsonConvert.SerializeObject(
+                self,
+                new JsonSerializerSettings
+                {
+                    ContractResolver = new DefaultContractResolver
+                    {
+                        NamingStrategy = new CamelCaseNamingStrategy()
+                    }
+                });
 
         public static StringContent ToJsonContent(this Type self) =>
             new(
