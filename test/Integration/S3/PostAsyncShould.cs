b9441352708746e9431ffa11b224df8e19745709
FantasticFiasco <mattias@kindb.org>
2026-01-05 11:09:23 +0100

    test: scaffold new integration tests

--- test/Integration/S3/PostAsyncShould.cs
+++ test/Integration/S3/PostAsyncShould.cs
@@ -18,21 +18,65 @@ namespace AwsSignatureVersion4.Integration.S3
         [Theory]
         [InlineData(IamAuthenticationType.User)]
         [InlineData(IamAuthenticationType.Role)]
-        public async Task ThrowNotSupportedException(IamAuthenticationType iamAuthenticationType)
+        public async Task SucceedMultipartUpload(IamAuthenticationType iamAuthenticationType)
         {
-            // Arrange
-            var bucketObject = new BucketObject(BucketObjectKey.WithoutPrefix);
+            // 1. Initiate the upload
+            // 2. Upload the object parts
+            //   - Part number between 1 and 10,000
+            //   - Record the part number and the ETag value
+            // 3. Complete the multipart upload
+            //   - Include the upload ID and a list of part numbers and their corresponding ETag values
 
-            // Act
-            var actual = HttpClient.PostAsync(
-                $"{Context.S3BucketUrl}/{bucketObject.Key}",
-                bucketObject.StringContent,
-                Context.RegionName,
-                Context.ServiceName,
-                ResolveMutableCredentials(iamAuthenticationType));
+            // Verification: Assert that all parts have been removed, and the combined object exists.
 
-            // Assert
-            await actual.ShouldThrowAsync<NotSupportedException>();
+            throw new NotImplementedException();
+        }
+
+        [Theory]
+        [InlineData(IamAuthenticationType.User)]
+        [InlineData(IamAuthenticationType.Role)]
+        public async Task AbortMultipartUpload(IamAuthenticationType iamAuthenticationType)
+        {
+            // 1. Initiate the upload
+            // 2. Upload some of the object parts
+            //   - Part number between 1 and 10,000
+            // 3. Abort the multipart upload
+
+            // Verification: Assert that all parts have been removed, and there is no combined object.
+
+            throw new NotImplementedException();
+        }
+
+        [Theory]
+        [InlineData(IamAuthenticationType.User)]
+        [InlineData(IamAuthenticationType.Role)]
+        public async Task ListOngoingMultipartUploads(IamAuthenticationType iamAuthenticationType)
+        {
+            // 1. Initiate the upload
+            // 2. Upload some of the object parts
+            //   - Part number between 1 and 10,000
+            // 3. List ongoing multipart uploads
+            // 4. Abort the multipart upload
+
+            // Verification: Assert that ongoing upload is listed.
+
+            throw new NotImplementedException();
+        }
+
+        [Theory]
+        [InlineData(IamAuthenticationType.User)]
+        [InlineData(IamAuthenticationType.Role)]
+        public async Task ListMultipartUploadParts(IamAuthenticationType iamAuthenticationType)
+        {
+            // 1. Initiate the upload
+            // 2. Upload some of the object parts
+            //   - Part number between 1 and 10,000
+            // 3. List multipart upload parts
+            // 4. Abort the multipart upload
+
+            // Verification: Assert that the uploaded parts are listed.
+
+            throw new NotImplementedException();
         }
     }
 }
