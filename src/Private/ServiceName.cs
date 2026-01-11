27531f8a92a3676175d222c4c1afeeee83021d33
FantasticFiasco <mattias@kindb.org>
2026-01-11 08:51:08 +0100

    test: add test for services requiring X-Amz-Content-SHA256 content header

--- src/Private/ServiceName.cs
+++ src/Private/ServiceName.cs
@@ -1,9 +1,9 @@
 ﻿namespace AwsSignatureVersion4.Private
 {
-    internal static class ServiceName
+    public static class ServiceName
     {
-        internal const string OpenSearchServerless = "aoss";
-        internal const string S3 = "s3";
-        internal const string VpcLattice = "vpc-lattice-svcs";
+        public const string OpenSearchServerless = "aoss";
+        public const string S3 = "s3";
+        public const string VpcLattice = "vpc-lattice-svcs";
     }
 }
